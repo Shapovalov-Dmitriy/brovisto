7055a656058cc6c501e2d2eb89a979de0c63d7d2
Dmitriy Shapovalov <shapovalov-84@internet.ru>
2024-03-13 17:42:36 +0700

    first_commit

---
+++ config/gulp-tasks/sprite.js
@@ -0,0 +1,52 @@
+import svgSprite from "gulp-svg-sprite";
+export const sprite = () => {
+	return app.gulp.src(`${app.path.src.svgicons}`, {})
+		.pipe(app.plugins.plumber(
+			app.plugins.notify.onError({
+				title: "SVG",
+				message: "Error: <%= error.message %>"
+			}))
+		)
+		.pipe(svgSprite({
+			mode: {
+				symbol: {
+					sprite: '../img/icons/icons.svg',
+					//example: true
+				}
+			},
+			shape: {
+				id: {
+					separator: '',
+					generator: 'svg-'
+				},
+				transform: [
+					{
+						svgo: {
+							plugins: [
+								{
+									name: 'preset-default',
+									params: {
+										overrides: {
+											// disable plugins
+											removeViewBox: false,
+										},
+									},
+								},
+								// { removeXMLNS: true },
+								// { convertPathData: false },
+								// { removeViewBox: false },
+							]
+						}
+					}
+				]
+			},
+			svg: {
+				rootAttributes: {
+					style: 'display: none;',
+					'aria-hidden': true
+				},
+				xmlDeclaration: false
+			}
+		}))
+		.pipe(app.gulp.dest(`${app.path.srcFolder}`));
+}
